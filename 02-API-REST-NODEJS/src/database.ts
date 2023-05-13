c6c0fd41fcddfcbf19c57a3b6bd1f5142adf8d19
Keith Dimeni <keithdimeni@gmail.com>
2023-05-13 06:15:55 -0300

    update: config database

--- 02-API-REST-NODEJS/src/database.ts
+++ 02-API-REST-NODEJS/src/database.ts
@@ -1,18 +1,19 @@
 import { knex as setupKex, Knex } from 'knex'
 import { env } from './env'
 
-const databaseClient = env.DATABASE_CLIENT == 'sqlite' ? {
-  filename: env.DATABASE_URL,
-} : env.DATABASE_URL
-
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection: databaseClient,
-},
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
     directory: './database/migrations',
   },
 }
+
 export const knex = setupKex(config)
